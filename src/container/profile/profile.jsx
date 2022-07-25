4c30d5ec08602e97f65e5f7c9b7bcaf61d8ef382
CHANG CHENG LIOU <defghl7563000@gmail.com>
2022-07-25 12:21:20 +0800

    changes

--- src/container/profile/profile.jsx
+++ src/container/profile/profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { FaPlay } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 // framer motion
